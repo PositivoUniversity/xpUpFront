4d1eb1f9b4e84dd8a029b4c0566ec932516c89ee
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2023-11-11 19:58:39 -0300

    trazendo os usuários para o modal de esição

--- src/screens/Admin.js
+++ src/screens/Admin.js
@@ -13,16 +13,18 @@ export default function Admin() {
     const [data, setData] = useState([]);
     const [isFABOpen, setFABOpen] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisibleEdit, setModalVisibleEdit] = useState(false);
     const [name, setName] = useState([]);
     const [email, setEmail] = useState([]);
     const [passwordTip, setPasswordTip] = useState([]);
     const [password, setPassword] = useState([]);
     const [course, setCourse] = useState([]);
-    const [selectedCourse, setSelectedCourse] = useState(null);
+    const [selectedCourse, setSelectedCourse] = useState('');
 
     const sendUser = () => {
         setModalVisible((value) => !value);
         sendUserData();
+        clearInputs();
     }
 
     const sendUserData = async () => {
@@ -43,13 +45,26 @@ export default function Admin() {
             console.error('Erro ao criar usuário:', error);
         }
     };
-
-
-
     const handleCreateUser = () => {
         setModalVisible((value) => !value);
     }
-
+    const handleEditUser = (selectedUser) => {
+        setModalVisibleEdit((value) => !value);
+        if (selectedUser) {
+            setName(selectedUser.name);
+            setEmail(selectedUser.email);
+            setPasswordTip(selectedUser.passwordTip);
+            setPassword(selectedUser.password);
+            setSelectedCourse(selectedUser.course);
+        }
+    }
+    const clearInputs = () => {
+        setName('');
+        setEmail('');
+        setPasswordTip('');
+        setPassword('');
+        setSelectedCourse('');
+    }
     useEffect(() => {
         const loadData = async () => {
             try {
@@ -94,10 +109,42 @@ export default function Admin() {
                         data={data}
                         textStyle={styles.styleText}
                         onpressDelete={() => console.log('Deletar Usuário Pressionado')}
-                        onpressEdit={() => ('Editar Usuário Pressionado')}
+                        onpressEdit={(user) => handleEditUser(user)}
                     />
-                    <DefaultModal isVisible={modalVisible} onClose={() => setModalVisible(!modalVisible)} sendData={sendUser}>
+                    <DefaultModal isVisible={modalVisible} onClose={() => { setModalVisible(!modalVisible); clearInputs(); }} sendData={sendUser}>
                         <Text>Cadastrar um Professor</Text>
+                        <DefaultInput label="Nome"
+                            value={name}
+                            onChangeText={setName}
+                        />
+                        <DefaultInput label="Email"
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                        <DefaultInput label="Dica de Senha"
+                            value={passwordTip}
+                            onChangeText={setPasswordTip}
+                        />
+                        <DefaultInput label="Senha"
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+                        <View style={styles.pickerContainer}>
+                            <Text>Curso</Text>
+                            <Picker
+                                selectedValue={selectedCourse}
+                                onValueChange={(itemValue) => setSelectedCourse(itemValue)}
+                                style={styles.picker}
+                            >
+                                <Picker.Item label="Selecione um curso" value="" />
+                                {course.map((courseItem) => (
+                                    <Picker.Item key={courseItem.id} label={courseItem.name} value={courseItem.id} />
+                                ))}
+                            </Picker>
+                        </View>
+                    </DefaultModal>
+                    <DefaultModal isVisible={modalVisibleEdit} onClose={() => { setModalVisibleEdit(!modalVisibleEdit); clearInputs(); }} sendData={handleEditUser}>
+                        <Text>Editar o Professor </Text>
                         <DefaultInput label="Nome"
                             value={name}
                             onChangeText={setName}
@@ -134,9 +181,8 @@ export default function Admin() {
             )}
             <FAB.Group
                 open={isFABOpen}
-                icon={data && data.length > 0 ? 'account' : 'plus'}
+                icon={data && data.length > 0 ? 'plus' : 'account'}
                 actions={[
-                    { icon: 'pencil', label: 'Editar Usuário', onPress: () => console.log('Editar Usuário Pressionado'), labelStyle: { color: 'white' } },
                     { icon: 'plus', label: 'Criar Novo Usuário', onPress: () => handleCreateUser(), labelStyle: { color: 'white' } },
                 ]}
                 onStateChange={({ open }) => setFABOpen(open)}
